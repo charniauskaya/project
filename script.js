f9828da4e6e5b3524ae55ca9c431255b00e822ed
charniauskaya <silchenko_aa@mail.ru>
2019-07-22 09:15:16 +0300

    add3pages

---
+++ script.js
@@ -0,0 +1,44 @@
+﻿/*('.carousel').carousel({
+    interval: 3000,
+    keyboard: false,
+    pause: 'hover',
+    ride: 'carousel',
+    wrap: false
+  });
+(function () {
+  // метод cycle
+  ('.btn').click(function () {
+    var action = (this).attr('data-action');
+    if (action.indexOf('to') >= 0) {
+      var action = parseInt(action.substring(3))-1;
+    }
+    ('#carousel').carousel(action);
+  });
+});
+
+//*/
+
+var modal = document.getElementById('modal');
+
+
+var btn = document.getElementById("modalbtn");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
