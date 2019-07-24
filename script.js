597059fa06c57f934e6b426b293fe578a3113787
charniauskaya <silchenko_aa@mail.ru>
2019-07-24 15:43:48 +0300

    finalProject

--- script.js
+++ script.js
@@ -1,44 +1,12 @@
-﻿/*('.carousel').carousel({
-    interval: 3000,
-    keyboard: false,
-    pause: 'hover',
-    ride: 'carousel',
-    wrap: false
+﻿function(){ 
+  ("#index_tab_trigger a").click(function(e){
+    e.preventDefault();
+    (this).tab('show');
   });
-(function () {
-  // метод cycle
-  ('.btn').click(function () {
-    var action = (this).attr('data-action');
-    if (action.indexOf('to') >= 0) {
-      var action = parseInt(action.substring(3))-1;
-    }
-    ('#carousel').carousel(action);
-  });
+};
+
+(function(){ 
+    // Отображается 1 вкладка, 
+    // т.к. отсчёт начинается с нуля
+    $("#myTab li:eq(0) a").tab('show');
 });
-
-//*/
-
-var modal = document.getElementById('modal');
-
-
-var btn = document.getElementById("modalbtn");
-
-
-var span = document.getElementsByClassName("close")[0];
-
-
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
